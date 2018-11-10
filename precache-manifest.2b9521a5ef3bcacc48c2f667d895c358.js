fd2d4cc326facf37430bf5d41fd998687054e78b
fonzaex <daniel.fnz@hotmail.com>
2018-11-10 17:00:03 -0200

    Updates

---
+++ precache-manifest.2b9521a5ef3bcacc48c2f667d895c358.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "20b17fe7cb2b6dc0fbcb",
+    "url": "/fe-2018/static/js/runtime~main.20b17fe7.js"
+  },
+  {
+    "revision": "586589bca20d6fc87093",
+    "url": "/fe-2018/static/js/main.586589bc.chunk.js"
+  },
+  {
+    "revision": "0a0b6c92351c499423cd",
+    "url": "/fe-2018/static/js/1.0a0b6c92.chunk.js"
+  },
+  {
+    "revision": "3e6dd97c18fb1be25dfc29fa267993fa",
+    "url": "/fe-2018/index.html"
+  }
+];
