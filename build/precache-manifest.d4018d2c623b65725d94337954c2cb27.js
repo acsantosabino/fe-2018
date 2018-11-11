0ba1a0905e6f77d50b7a08defa3e3c64da5e54c7
fonzaex <daniel.fnz@hotmail.com>
2018-11-11 19:15:26 -0200

    Correções

---
+++ build/precache-manifest.d4018d2c623b65725d94337954c2cb27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20b17fe7cb2b6dc0fbcb",
+    "url": "/fe-2018/static/js/runtime~main.20b17fe7.js"
+  },
+  {
+    "revision": "d9603d5c6d90049e786f",
+    "url": "/fe-2018/static/js/main.d9603d5c.chunk.js"
+  },
+  {
+    "revision": "88a66241d3c75d0f7928",
+    "url": "/fe-2018/static/js/1.88a66241.chunk.js"
+  },
+  {
+    "revision": "88a66241d3c75d0f7928",
+    "url": "/fe-2018/static/css/1.b00aedc5.chunk.css"
+  },
+  {
+    "revision": "ec4446b53e342c60119045e7cc93d679",
+    "url": "/fe-2018/index.html"
+  }
+];
