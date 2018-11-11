0f892717c953f836562ac5a6f6f3b2eb4e813c6b
fonzaex <daniel.fnz@hotmail.com>
2018-11-11 18:51:28 -0200

    Cadastro, edição, exclusão e listagem de nomes

--- src/nomes/nomes.js
+++ src/nomes/nomes.js
@@ -1,29 +1,132 @@
-import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import Input from "@material-ui/core/Input";
-import FormControl from "@material-ui/core/FormControl";
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import React from 'react';
 
 export default class Nomes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 0,
+      nome: "",
+      sobrenome: "",
+      titulo: "",
+      sufixo: "",
       nomePreferido: "Sim",
-      usoNome: 1,
-      condicional: 1
+      usoNome: "Relatório(R)",
+      condicional: "Informação não confiável",
+      editando: false,
+      nomes: []
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addNome = this.addNome.bind(this);
+    this.updateNome = this.updateNome.bind(this);
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  addNome() {
+
+    if(this.state.nome == null || this.state.nome == '') {
+      return alert("O campo 'nome' está vazio");
+    }
+
+    let id = new Date().getTime();
+    this.state.nomes.push({
+      id: id,
+      nome: this.state.nome,
+      sobrenome: this.state.sobrenome,
+      titulo: this.state.titulo,
+      sufixo: this.state.sufixo,
+      usoNome: this.state.usoNome,
+      nomePreferido: this.state.nomePreferido,
+      condicional: this.state.condicional
+    });
+    sessionStorage.setItem("nomes", JSON.stringify(this.state.nomes));
+    this.clear();
+  }
+
+  deleteNome(id) {
+    this.state.nomes.splice(this.state.nomes.findIndex(nome => nome.id === id),1);
+    this.setState({ nomes: this.state.nomes });
+    sessionStorage.setItem("nomes", JSON.stringify(this.state.nomes));
+  }
+
+  clear() {
+    this.setState({
+      nome: "",
+      usoNome: "",
+      sobrenome: "",
+      sufixo: "",
+      titulo: ""
+    });
+  }
+
+  editNome(nome) {
+    this.setState({
+      id: nome.id,
+      nome: nome.nome,
+      sobrenome: nome.sobrenome,
+      titulo: nome.titulo,
+      sufixo: nome.sufixo,
+      nomePreferido: nome.nomePreferido,
+      usoNome: nome.usoNome,
+      condicional: nome.condicional,
+      editando: true
+    });
+    sessionStorage.setItem("nomes", JSON.stringify(this.state.nomes));
+  }
+
+  updateNome() {
+    this.state.nomes[this.state.nomes.findIndex(nome => nome.id === this.state.id)] = {
+      id: this.state.id,
+      nome: this.state.nome,
+      sobrenome: this.state.sobrenome,
+      titulo: this.state.titulo,
+      sufixo: this.state.sufixo,
+      nomePreferido: this.state.nomePreferido,
+      usoNome: this.state.usoNome,
+      condicional: this.state.condicional
+    };
+    this.clear();
+    this.setState({ nomes: this.state.nomes, editando: false });
+    sessionStorage.setItem("nomes", JSON.stringify(this.state.nomes));
+  }
+
+  componentDidMount() {
+    this.setState({
+      nomes: JSON.parse(sessionStorage.getItem("nomes"))
+        ? JSON.parse(sessionStorage.getItem("nomes"))
+        : []
+    });
   }
 
   render() {
     return (
       <React.Fragment>
         <Typography variant="h6" gutterBottom>
-          Nomes
+          Nome
         </Typography>
+
         <Grid container spacing={24}>
           <Grid item xs={12} sm={6}>
             <TextField
@@ -31,8 +134,9 @@ export default class Nomes extends React.Component {
               id="nome"
               name="nome"
               label="Nome"
+              value={this.state.nome}
+              onChange={this.handleChange("nome")}
               fullWidth
-              autoComplete="nome"
             />
           </Grid>
           <Grid item xs={12} sm={6}>
@@ -41,8 +145,9 @@ export default class Nomes extends React.Component {
               id="sobrenome"
               name="sobrenome"
               label="Sobrenome"
+              value={this.state.sobrenome}
+              onChange={this.handleChange("sobrenome")}
               fullWidth
-              autoComplete="sobrenome"
             />
           </Grid>
           <Grid item xs={12} sm={6}>
@@ -51,8 +156,9 @@ export default class Nomes extends React.Component {
               id="titulo"
               name="titulo"
               label="Título"
+              value={this.state.titulo}
+              onChange={this.handleChange("titulo")}
               fullWidth
-              autoComplete="titulo"
             />
           </Grid>
           <Grid item xs={12} sm={6}>
@@ -60,8 +166,9 @@ export default class Nomes extends React.Component {
               id="sufixo"
               name="sufixo"
               label="Sufixo"
+              value={this.state.sufixo}
+              onChange={this.handleChange("sufixo")}
               fullWidth
-              autoComplete="sufixo"
             />
           </Grid>
           <Grid item xs={12} sm={6}>
@@ -74,6 +181,7 @@ export default class Nomes extends React.Component {
               <InputLabel htmlFor="nomePreferido">Nome Preferido</InputLabel>
               <Select
                 value={this.state.nomePreferido}
+                onChange={this.handleChange("nomePreferido")}
                 input={<Input name="nomePreferido" id="nomePreferido" />}
                 displayEmpty
               >
@@ -94,17 +202,27 @@ export default class Nomes extends React.Component {
               <Select
                 value={this.state.usoNome}
                 input={<Input name="usoNome" id="usoNome" />}
+                onChange={this.handleChange("usoNome")}
                 displayEmpty
               >
-                <MenuItem value={1}>Relatório(R)</MenuItem>
-                <MenuItem value={2}>Nome de recém-nascido(N)</MenuItem>
-                <MenuItem value={3}>Nome profissional ou comercial(C)</MenuItem>
-                <MenuItem value={4}>
-                  Nome de solteiro, nome de nascimento ou nome
-                  original(Original)
+                <MenuItem value={"Relatório(R)"}>Relatório(R)</MenuItem>
+                <MenuItem value={"Nome de recém-nascido(N)"}>
+                  Nome de recém-nascido(N)
                 </MenuItem>
-                <MenuItem value={5}>Nome registrado (nome legal)</MenuItem>
-                <MenuItem value={8}>Outro nome (alias)</MenuItem>
+                <MenuItem value={"Nome de recém-nascido(N)"}>
+                  Nome profissional ou comercial(C)
+                </MenuItem>
+                <MenuItem
+                  value={"Nome de solteiro, nome de nascimento(Original))"}
+                >
+                  Nome de solteiro, nome de nascimento(Original)
+                </MenuItem>
+                <MenuItem value={"Nome registrado (nome legal)"}>
+                  Nome registrado (nome legal)
+                </MenuItem>
+                <MenuItem value={"Outro nome (alias)"}>
+                  Outro nome (alias)
+                </MenuItem>
               </Select>
             </FormControl>
           </Grid>
@@ -123,19 +241,107 @@ export default class Nomes extends React.Component {
                 value={this.state.condicional}
                 input={<Input name="condicional" id="condicional" />}
                 displayEmpty
+                onChange={this.handleChange("condicional")}
               >
-                <MenuItem value={1}>Informação não confiável </MenuItem>
-                <MenuItem value={2}>Nome com erro de digitação</MenuItem>
-                <MenuItem value={3}>Nome para não ser usado</MenuItem>
-                <MenuItem value={4}>Vínculo do nome proibido por lei</MenuItem>
-                <MenuItem value={6}>
+                <MenuItem value={"Informação não confiável"}>
+                  Informação não confiável
+                </MenuItem>
+                <MenuItem value={"Nome com erro de digitação"}>
+                  Nome com erro de digitação
+                </MenuItem>
+                <MenuItem value={"Nome para não ser usado"}>
+                  Nome para não ser usado
+                </MenuItem>
+                <MenuItem value={"Vínculo do nome proibido por lei"}>
+                  Vínculo do nome proibido por lei
+                </MenuItem>
+                <MenuItem value={"Requisito de privacidade/segurança especial"}>
                   Requisito de privacidade/segurança especial
                 </MenuItem>
-                <MenuItem value={9}>Nome temporário</MenuItem>
+                <MenuItem value={"Nome temporário"}>Nome temporário</MenuItem>
               </Select>
             </FormControl>
           </Grid>
+          <Grid item xs={12} sm={12} container justify="flex-end">
+            {!this.state.editando && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.addNome}
+              >
+                Adicionar nome
+              </Button>
+            )}
+            {this.state.editando && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.updateNome}
+              >
+                Salvar edição
+              </Button>
+            )}
+          </Grid>
         </Grid>
+
+        {this.state.nomes.length > 0 && (
+          <Grid>
+            <Typography variant="h6" gutterBottom>
+              Nomes cadastrados
+            </Typography>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Nome</TableCell>
+                  <TableCell>Uso do Nome</TableCell>
+                  <TableCell>Nome Preferido</TableCell>
+                  <TableCell>Indicador de Uso</TableCell>
+                  <TableCell>Ações</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.nomes.map(row => {
+                  return (
+                    <TableRow key={row.id}>
+                      <TableCell component="th" scope="row">
+                        {row.titulo +
+                          " " +
+                          row.nome +
+                          " " +
+                          row.sobrenome +
+                          " " +
+                          row.sufixo}
+                      </TableCell>
+                      <TableCell>{row.usoNome}</TableCell>
+                      <TableCell>{row.nomePreferido}</TableCell>
+                      <TableCell>{row.condicional}</TableCell>
+                      <TableCell>
+                        <IconButton
+                          aria-label="Edit"
+                          color="primary"
+                          onClick={() => {
+                            this.editNome(row);
+                          }}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton
+                          aria-label="Delete"
+                          color="secondary"
+                          onClick={() => {
+                            this.deleteNome(row.id);
+                          }}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Grid>
+        )}
       </React.Fragment>
     );
   }
