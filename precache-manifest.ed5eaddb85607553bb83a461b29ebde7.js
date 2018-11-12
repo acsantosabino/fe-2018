3f1ebf472df93936ec3d2c6c459651d6e9ce33bb
Danillo Guimarães <danillo@danilloguimaraes.com.br>
2018-11-12 05:00:42 -0200

    Updates

---
+++ precache-manifest.ed5eaddb85607553bb83a461b29ebde7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "20b17fe7cb2b6dc0fbcb",
+    "url": "/fe-2018/static/js/runtime~main.20b17fe7.js"
+  },
+  {
+    "revision": "49705e0c6ea02c4d7ecc",
+    "url": "/fe-2018/static/js/main.49705e0c.chunk.js"
+  },
+  {
+    "revision": "b88984134dc79c49ab58",
+    "url": "/fe-2018/static/js/1.b8898413.chunk.js"
+  },
+  {
+    "revision": "a9115daee40e43ea3680179ab507443d",
+    "url": "/fe-2018/index.html"
+  }
+];
