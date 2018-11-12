4f122847da9cb352ee2579f207c1f29bcee0e48b
Arthur Santos <acsantosabino@gmail.com>
2018-11-11 23:39:50 -0200

    Updates

---
+++ precache-manifest.7dbfaa5108d37cbf6d39c750d0253d89.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "20b17fe7cb2b6dc0fbcb",
+    "url": "/fe-2018/static/js/runtime~main.20b17fe7.js"
+  },
+  {
+    "revision": "4a82dbde93b76609a02a",
+    "url": "/fe-2018/static/js/main.4a82dbde.chunk.js"
+  },
+  {
+    "revision": "13b0465f49f389519971",
+    "url": "/fe-2018/static/js/1.13b0465f.chunk.js"
+  },
+  {
+    "revision": "e145586661b73bfde6b4bc73a40948bf",
+    "url": "/fe-2018/index.html"
+  }
+];
